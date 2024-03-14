e35ec42aa94c01ccbec146550a6f9a5d00a0d715
Lubern5 <99304345+Lubern5@users.noreply.github.com>
2024-03-14 12:30:33 -0400

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "lu.123" # Replace with your actual S3 bucket name
+    bucket = "reddit-clone-k8s" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-2"
   }
